ee7c8e7fff3dc4034802cd87d3f22a80e6230151
Domanski, Adam <adam.domanski_external@immobilienscout24.de>
2015-02-25 14:04:53 +0100

    hacked width

--- dist/ng-sortable.js
+++ dist/ng-sortable.js
@@ -672,7 +672,7 @@
 
             placeHolder = angular.element($document[0].createElement(tagName))
                 .addClass(sortableConfig.placeHolderClass).addClass(scope.sortableScope.options.additionalPlaceholderClass);
-            placeHolder.css('width', $helper.width(scope.itemScope.element) + 'px');
+            placeHolder.css('width', ($helper.width(scope.itemScope.element) - 1) + 'px');
             placeHolder.css('height', $helper.height(scope.itemScope.element) + 'px');
 
             placeElement = angular.element($document[0].createElement(tagName));
